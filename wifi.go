cf44c8eb206a2df13e86741532967e2d1d13a31d
Alexey Makhov <amakhov@avito.ru>
2016-02-17 09:19:42 +0300

    gofmt

--- wifi.go
+++ wifi.go
@@ -2,90 +2,90 @@ package main
 
 // import "fmt"
 import (
-    "time"
-    "os/exec"
-    "log"
-    "os"
-    "github.com/codegangsta/cli"
-    "strings"
+	"github.com/codegangsta/cli"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
 )
 
 func main() {
-  app := cli.NewApp()
-  app.Name = "Go Wifi checker"
-  app.Usage = ""
-  app.HideVersion = true
+	app := cli.NewApp()
+	app.Name = "Go Wifi checker"
+	app.Usage = ""
+	app.HideVersion = true
 
-  var timeout string
-  app.Flags = []cli.Flag {
-    cli.StringFlag{
-      Name:        "timeout, t",
-      Value:       "2s",
-      Usage:       "timeout between checks",
-      Destination: &timeout,
-    },
-  }
+	var timeout string
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "timeout, t",
+			Value:       "2s",
+			Usage:       "timeout between checks",
+			Destination: &timeout,
+		},
+	}
 
-  app.Action = func(c *cli.Context) {
-    loop(timeout)
-  }
+	app.Action = func(c *cli.Context) {
+		loop(timeout)
+	}
 
-  app.Run(os.Args)
+	app.Run(os.Args)
 }
 
 func loop(timeout string) {
 
-    sleep := checkParams(timeout)
-    eth := getWifiInterface()
+	sleep := checkParams(timeout)
+	eth := getWifiInterface()
 
-    log.Printf("Start checking interface %s with timeout %s", eth, sleep.String())
+	log.Printf("Start checking interface %s with timeout %s", eth, sleep.String())
 
-    for {
+	for {
 
-        enabled, err := exec.Command("networksetup", "getairportpower", eth).Output()
+		enabled, err := exec.Command("networksetup", "getairportpower", eth).Output()
 
-        if strings.Contains(strings.Trim(string(enabled), "\n "), "On") {
+		if strings.Contains(strings.Trim(string(enabled), "\n "), "On") {
 
-            err = exec.Command("ping", "-c", "2", "8.8.8.8").Run()
+			err = exec.Command("ping", "-c", "2", "8.8.8.8").Run()
 
-            if err != nil {
-                restartWifi(string(eth))
-            }
+			if err != nil {
+				restartWifi(string(eth))
+			}
 
-            time.Sleep(sleep)
-        }
-    }
+			time.Sleep(sleep)
+		}
+	}
 
 }
 
 func checkParams(timeout string) time.Duration {
-    sleep, err := time.ParseDuration(timeout)
+	sleep, err := time.ParseDuration(timeout)
 
-    if err != nil {
-        log.Fatalln("Incorrect usage of --timeout param; Pls use like --timeout=5s")
-    }
+	if err != nil {
+		log.Fatalln("Incorrect usage of --timeout param; Pls use like --timeout=5s")
+	}
 
-    if sleep < time.Duration(time.Second) {
-        log.Fatal("Timeout can't be less than 1s")
-    }
+	if sleep < time.Duration(time.Second) {
+		log.Fatal("Timeout can't be less than 1s")
+	}
 
-    return sleep
+	return sleep
 }
 
 func getWifiInterface() string {
-    cmd := "networksetup -listallhardwareports | fgrep Wi-Fi -A1 | awk 'NF==2{print $2}'"
-    eth, err := exec.Command("bash", "-c", cmd).Output()
+	cmd := "networksetup -listallhardwareports | fgrep Wi-Fi -A1 | awk 'NF==2{print $2}'"
+	eth, err := exec.Command("bash", "-c", cmd).Output()
 
-    if err != nil {
-        log.Fatal("Can't detect wifi interface")
-    }
+	if err != nil {
+		log.Fatal("Can't detect wifi interface")
+	}
 
-    return strings.Trim(string(eth), " \n")
+	return strings.Trim(string(eth), " \n")
 }
 
 func restartWifi(eth string) {
-    log.Println("Wifi restarting")
-    exec.Command("networksetup", "-setairportpower", eth, "off").Run()
-    time.Sleep(2 * time.Second)
-    exec.Command("networksetup", "-setairportpower", eth, "on").Run()
+	log.Println("Wifi restarting")
+	exec.Command("networksetup", "-setairportpower", eth, "off").Run()
+	time.Sleep(2 * time.Second)
+	exec.Command("networksetup", "-setairportpower", eth, "on").Run()
 }
